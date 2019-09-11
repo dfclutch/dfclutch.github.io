a7f466a17e68ec17a06749ad7caf6526a91fb1dc
dfclutch <filler.7@osu.edu>
2019-09-10 20:00:38 -0400

    updated graphs.html to fix wrong import of jquery

--- apps/automata_src/automata.js
+++ apps/automata_src/automata.js
@@ -5,69 +5,71 @@
 */
 
 function Automata(state) {
-	this.Q = [];
-	this.D = [];
-	this.Q_open = true;
-	this.D_open = false;
-	this.next_state_index = 1;
-	this.Q.push (new QState(
-		0,
-		{
-			x: 40,
-			y: state.canvas.height / 2
-		}
-	));
+    this.Q = [];
+    this.D = [];
+    this.Q_open = true;
+    this.D_open = false;
+    this.next_state_index = 1;
+    this.Q.push(new QState(
+        0,
+        {
+            x: 40,
+            y: state.canvas.height / 2
+        }
+    ));
 
-	this.draw = () => {
-		graphics.clear_canvas(state.canvas, state.context);
-		this.Q.forEach(q_state => {
-			q_state.draw();
-		});
-		this.D.forEach(transition => {
-			transition.draw();
-		});
-	};
+    this.draw = () => {
+        graphics.clear_canvas(state.canvas, state.context);
+        this.Q.forEach(q_state => {
+            q_state.draw();
+        });
+        this.D.forEach(transition => {
+            transition.draw();
+        });
+    };
 
-	this.add_state = (coord) => {
-		let new_state = new QState(
-			this.next_state_index,
-			coord
-		);
-		this.next_state_index++;
-		this.Q.push(new_state);
-		this.draw();
-	};
+    this.add_state = (coord) => {
+        let new_state = new QState(
+            this.next_state_index,
+            coord
+        );
+        this.next_state_index++;
+        this.Q.push(new_state);
+        this.draw();
+    };
 
-	this.delete_state = (coord) => {
-		state.automata.Q = state.automata.Q.filter(q_state => {
-			const in_x_range = coord.x < q_state.coord.x + NODE_RADIUS && coord.x > q_state.coord.x - NODE_RADIUS;
-			const in_y_range = coord.y < q_state.coord.y + NODE_RADIUS && coord.y > q_state.coord.y - NODE_RADIUS;
-			return !(in_x_range && in_y_range);
-		});
-		console.log(state.automata.Q);
-		this.draw();
-	};
+    this.delete_state = (coord) => {
+        state.automata.Q = state.automata.Q.filter(q_state => !this._click_did_hit(coord, q_state));
 
-	this.toggle_lock_Q = () => {
-		this.Q_open = !this.Q_open;
-	};
+        this.draw();
+    };
 
-	this.toggle_lock_d = () => {
-		this.D_open = !this.D_open;
-	};
+    this.toggle_lock_Q = () => {
+        this.Q_open = !this.Q_open;
+    };
 
-	this.next_state = (input) => {
-		return input;
-	}
+    this.toggle_lock_d = () => {
+        this.D_open = !this.D_open;
+    };
+
+    this.next_state = (input) => {
+        return input;
+    };
+
+    this._click_did_hit = (coord, q_state) => {
+        const in_x_range = coord.x < q_state.coord.x + NODE_RADIUS && coord.x > q_state.coord.x - NODE_RADIUS;
+        const in_y_range = coord.y < q_state.coord.y + NODE_RADIUS && coord.y > q_state.coord.y - NODE_RADIUS;
+        return in_x_range && in_y_range;
+    };
 }
 
 
 function get_click_coord_in_canvas(event) {
-	const rect = state.canvas.getBoundingClientRect()
+    const rect = state.canvas.getBoundingClientRect()
     const x = event.clientX - rect.left
     const y = event.clientY - rect.top
     return {
-    	x,
-    	y
+        x,
+        y
     };
 }
