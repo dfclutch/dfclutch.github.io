937a5f2bd28a9f6e3b23faae054ceebc30d06e82
dfclutch <filler.7@osu.edu>
2020-04-16 10:53:03 -0400

    fixed typo

--- new_site/src/Content/home.js
+++ new_site/src/Content/home.js
@@ -18,7 +18,7 @@ class HomeContent extends React.Component {
 
                 <p>
                     Pennsylvania native living in Columbus, Ohio. Actively looking for employment in full-stack or
-                    front-end web development. Passionate about Network Science, Agrotech, and Artificial Intelligence."
+                    front-end web development. Passionate about Network Science, Agrotech, and Artificial Intelligence.
                 </p>
                 <p>
                     Frameworks and tech I use: React, Express, Hapi.js, Flask, Mongo, Axios, D3js, three.js, Webpack,
