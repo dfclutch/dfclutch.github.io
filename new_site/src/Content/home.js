98972a4bc81ed262bf7069404f8c300d4be0f283
dfclutch <filler.7@osu.edu>
2020-05-23 10:29:14 -0400

    added postman to skills page

--- new_site/src/Content/home.js
+++ new_site/src/Content/home.js
@@ -14,18 +14,18 @@ class HomeContent extends React.Component {
                 </p>
                 <div className="gallery">
                     <img src="./img/react.png" alt="React" title="ReactJs"/>
+                    <img src="./img/redux.png" alt="Redux" title="Redux"/>
                     <img src="./img/node.png" alt="Node" title="NodeJs"/>
                     <img src="./img/python.png" alt="Python" title="Python"/>
                     <img src="./img/mongo.png" alt="MongoDb" title="MongoDb"/>
                     <img src="./img/webpack.png" alt="Webpack" title="Webpack"/>
                     <img src="./img/d3.png" alt="D3js" title="D3js"/>
                     <img src="./img/git.png" alt="Git" title="Git"/>
-                    <img src="./img/jwt.png" alt="JWT" title="JSON Web Tokens"/>
+                    <img src="./img/postman.svg" alt="postman" title="Postman"/>
                     <img src="./img/sass.png" alt="Sass" title="Sass"/>
                     <img src="./img/npm.png" alt="npm" title="Node Package Manager"/>
                     <img src="./img/babel.jpg" alt="Babel" title="Babel"/>
                     <img src="./img/intellij.png" alt="Intellij" title="IntelliJ Idea"/>
-                    <img src="./img/redux.png" alt="Redux" title="Redux"/>
                 </div>
             </div>
         );
