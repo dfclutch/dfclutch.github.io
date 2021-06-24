fb06955ab078dd5434143639e9cde9cc62bd88e9
Dan Filler <dwfiller@gmail.com>
2021-06-24 12:23:37 -0400

    reword

--- blog/src/posts/8-formidable-components-article.js
+++ blog/src/posts/8-formidable-components-article.js
@@ -3,7 +3,7 @@ import React from "react";
 const content = (
   <>
     <p>
-      This article from <a href='https://formidable.com/'>Formidable</a> goes through some of the history of React design preferences and where they've landed in 2021.
+      This article from <a href='https://formidable.com/'>Formidable</a> goes through some of the history of React component structure preferences and where they've landed in 2021.
     </p>
     <a href='https://formidable.com/blog/2021/react-components/'>Not All Components are Created Equal</a>
   </>
