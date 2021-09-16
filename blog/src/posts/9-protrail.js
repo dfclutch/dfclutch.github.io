4ceaea90ab43b6340b7301e7a014c41d98c3c2b8
Dan Filler <daniel.filler@spiff.com>
2021-09-16 09:40:12 -0400

    fix link

---
+++ blog/src/posts/9-protrail.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default {
+  title: "Tarptent Protrail",
+  id: "protrail",
+  previewContent: "This is a classic design from tarptent that still strikes an excellent middle ground for ultralight tents.",
+  date: "Aug 1, 2021",
+  content: (
+    <>
+      <p>
+        
+      </p>
+    </>
+  ),
+};
