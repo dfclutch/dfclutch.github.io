668220fee51181cd8880fad8b3c4f62a3a4cc20f
Dan Filler <daniel.filler@spiff.com>
2021-10-15 10:46:26 -0400

    Add bocklin article

--- blog/src/posts/9-bocklin.js
+++ blog/src/posts/9-bocklin.js
@@ -5,19 +5,21 @@ export default {
   title: <>Arnold B&#246;cklin</>,
   id: "bocklin",
   previewContent:
-    "In which I realize how hard it is to find the name of a font.",
+    "In which I realize how hard it is to find the name of a typeface.",
   date: "Sept 18, 2021",
   content: (
     <>
       <p>
-        I feel like I know fonts better than most people. I work with font
-        almost every day as a part of my job. I can recognize a lot of fonts, I know the terms like <em>ascenders</em> and <em></em>
-        Armed with a college degree in Googling, I thought finding the name of a
-        familiar font would be a trivial quest. I went several months unable to
-        track down the name of a certain font. Every so often I'd see a new
-        example or think of a new adjective to search. Further, the font is
-        extremely distinctive and decorative. Below are some examples and
-        variants I found.
+        I feel like I know typography better than most people. I work with type
+        almost every day as a part of my job. I can recognize a lot of
+        typefaces, I know the terms like <em>ascenders</em> and <em>kerning</em>
+        . Even with all that, I was completely stumped trying to identify a
+        familiar font that I never new the name of. Every so often I'd see a new
+        example or think of a new adjective to search, but it took forever to
+        actually get a result.
+      </p>
+      <p>
+        Here's some examples of variations and similar typefaces I've found:
       </p>
       <Figure
         caption={<>Short North Tavern, Columbus, OH</>}
@@ -35,8 +37,8 @@ export default {
         breakpoints={[{ w: Infinity, v: "500px" }]}
       />
       <Figure
-        caption="That 70s Show title card"
-        src="https://ye-olde-blog.s3.amazonaws.com/img/bocklin/That_'70s_Show_logo.png"
+        caption="Moon River - Frank Ocean"
+        src="https://ye-olde-blog.s3.amazonaws.com/img/bocklin/moon-river.jpeg"
         breakpoints={[{ w: Infinity, v: "700px" }]}
       />
       <Figure
@@ -46,16 +48,66 @@ export default {
       />
 
       <p>
-        Of these, the Grateful Dead tee is the only true use of the font (the
-        others are variations). I finally broke through and found it when I
-        searched for "grateful dead font" It's called{" "}
-        <em>Arnold B&#246;cklin.</em>
+        Of these, the Grateful Dead tee is the only true use of the typeface,
+        the others are variations. After a few months of searching, I finally
+        got the name: <em>Arnold B&#246;cklin.</em>
       </p>
 
       <Figure
         caption=""
         src="https://ye-olde-blog.s3.amazonaws.com/img/bocklin/ArnoldBocklinSpec.png"
         breakpoints={[{ w: Infinity, v: "700px" }]}
+      />
+
+      <p>
+        It's a classic{" "}
+        <a
+          href="https://en.wikipedia.org/wiki/Display_typeface"
+          target="_blank"
+        >
+          Display typeface
+        </a>{" "}
+        - something used mainly for signs and titles, never for body text.
+        B&#246;cklin was designed in 1904 and named for an eponymous 19th century
+        Symbolist painter. Not sure exactly the connection between him and the
+        typeface/type designer. B&#246;cklin is an <em>Art Nouveau</em> style
+        typeface. The style focuses on highly decorative and artistic script
+        fonts, often featuring extended serifs. For me, it evokes thoughts of
+        high fantasy novels or psychadelics. Reading about the Art Nouveau
+        movement led me to this amazing Wikipedia page:{" "}
+        <a
+          href="https://en.wikipedia.org/wiki/Art_Nouveau_posters_and_graphic_arts"
+          target="_blank"
+        >
+          Art Nouveau posters and graphics
+        </a>
+        . None of the images contained font strikingly similar to B&#246;cklin,
+        but they are classic hipster wall decor:
+      </p>
+
+      <Figure
+        caption=""
+        src="https://upload.wikimedia.org/wikipedia/commons/8/80/Privat-Livemont_-_Absinthe_Robette%2C_1896.jpg"
+        breakpoints={[{ w: Infinity, v: "650px" }]}
+      />
+
+      <p>
+        Speaking of High Fantasy, here's a cool Art Nouveau typeface named for Tolkein's character Galadriel. Though it's contemporary (1970s), it is very similar to many of the fonts used in the Art Nouveau graphics:
+      </p>
+      <Figure
+        caption=""
+        src="https://ye-olde-blog.s3.amazonaws.com/img/bocklin/galadriel.jpeg"
+        breakpoints={[{ w: Infinity, v: "650px" }]}
+      />
+
+      <p>
+        Which is quite similar to, though a bit less creepy than, the typeface that American Horror Story uses: <a href="https://en.wikipedia.org/wiki/Willow_(typeface)" target="_blank">Willow</a>. Both feature high vertically position cross-members, but Willow has the tiny "O" character. <em>*shudder*</em>
+      </p>
+
+      <Figure
+        caption=""
+        src="https://ye-olde-blog.s3.amazonaws.com/img/bocklin/willow.png"
+        breakpoints={[{ w: Infinity, v: "650px" }]}
       />
     </>
   ),
