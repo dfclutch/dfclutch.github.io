be5a1b8494cf46153da499436bc05ed1ccc96021
Dan Filler <daniel.filler@spiff.com>
2022-07-07 10:06:38 -0400

    fix mono size

--- blog/src/post-components/Mono.js
+++ blog/src/post-components/Mono.js
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
-import colors from '../theme/colors';
+import colors from "../theme/colors";
+import typography from "../theme/typography";
 
 export const Mono = styled.span`
-  font-size: .9rem;
+  font-size: ${typography.small};
   font-family: monospace;
   background-color: ${colors.lightGrey}
   color: black; 
