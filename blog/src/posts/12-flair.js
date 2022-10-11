855629b0aa015f26fba7537b1dab16841a5d4c74
Dan Filler <daniel.filler@spiff.com>
2022-10-10 21:18:19 -0400

    update flair article

--- blog/src/posts/12-flair.js
+++ blog/src/posts/12-flair.js
@@ -10,18 +10,19 @@ export default {
   date: "Jul 8, 2022",
   content: (
     <>
+      <p>
+        I started this article over a year ago before I got my espresso machine.
+        I had used the Flair Pro manual press espresso machine every day for
+        more than 2 years, and I perfected the workflow using it. Not including
+        time for the kettle to boil and a bit of cleanup at the end, it takes
+        about 5 mins to make an espresso drink with frothed milk when this
+        workflow is perfected.
+      </p>
+      It all starts with boiling some water in my (busted) kettle.
       <Figure
         src="https://ye-olde-blog.s3.amazonaws.com/img/flair/1.jpg"
         breakpoints={[
-          { w: 1000, v: "80%" },
-          { w: 2000, v: "65%" },
-          { w: Infinity, v: "600px" },
-        ]}
-      />
-      It all starts with boiling some water in my (busted) kettle.
-      <Figure
-        src="https://ye-olde-blog.s3.amazonaws.com/img/flair/2.jpg"
-        breakpoints={[
+          { w: 800, v: "100%" },
           { w: 1000, v: "80%" },
           { w: 2000, v: "65%" },
           { w: Infinity, v: "600px" },
@@ -31,8 +32,9 @@ export default {
       water, and bring it back to a boil over my smallest burner. I use the
       single-size moka pot for this application.
       <Figure
-        src="https://ye-olde-blog.s3.amazonaws.com/img/flair/3.jpg"
+        src="https://ye-olde-blog.s3.amazonaws.com/img/flair/2.jpg"
         breakpoints={[
+          { w: 800, v: "100%" },
           { w: 1000, v: "80%" },
           { w: 2000, v: "65%" },
           { w: Infinity, v: "600px" },
@@ -41,8 +43,9 @@ export default {
       While the moka pot comes to a boil, I lay out all my accoutrements in the
       ready position.
       <Figure
-        src="https://ye-olde-blog.s3.amazonaws.com/img/flair/4.jpg"
+        src="https://ye-olde-blog.s3.amazonaws.com/img/flair/3.jpg"
         breakpoints={[
+          { w: 800, v: "100%" },
           { w: 1000, v: "80%" },
           { w: 2000, v: "65%" },
           { w: Infinity, v: "600px" },
@@ -52,8 +55,9 @@ export default {
       pot to warm it up (read: get it burning hot) while I do the next few prep
       steps.
       <Figure
-        src="https://ye-olde-blog.s3.amazonaws.com/img/flair/5.jpg"
+        src="https://ye-olde-blog.s3.amazonaws.com/img/flair/4.jpg"
         breakpoints={[
+          { w: 800, v: "100%" },
           { w: 1000, v: "80%" },
           { w: 2000, v: "65%" },
           { w: Infinity, v: "600px" },
@@ -63,8 +67,9 @@ export default {
       microwave. I can eyeball the amount of milk for a single cortado pretty
       easily after making a few hundred of them.
       <Figure
-        src="https://ye-olde-blog.s3.amazonaws.com/img/flair/6.jpg"
+        src="https://ye-olde-blog.s3.amazonaws.com/img/flair/5.jpg"
         breakpoints={[
+          { w: 800, v: "100%" },
           { w: 1000, v: "80%" },
           { w: 2000, v: "65%" },
           { w: Infinity, v: "600px" },
@@ -72,8 +77,9 @@ export default {
       />
       Weigh out the beans. 18.1g, close enough!
       <Figure
-        src="https://ye-olde-blog.s3.amazonaws.com/img/flair/7.jpg"
+        src="https://ye-olde-blog.s3.amazonaws.com/img/flair/6.jpg"
         breakpoints={[
+          { w: 800, v: "100%" },
           { w: 1000, v: "80%" },
           { w: 2000, v: "65%" },
           { w: Infinity, v: "600px" },
@@ -84,8 +90,9 @@ export default {
       the super hot metal) and warms the copper mount, increasing the thermal
       stability later.
       <Figure
-        src="https://ye-olde-blog.s3.amazonaws.com/img/flair/8.jpg"
+        src="https://ye-olde-blog.s3.amazonaws.com/img/flair/7.jpg"
         breakpoints={[
+          { w: 800, v: "100%" },
           { w: 1000, v: "80%" },
           { w: 2000, v: "65%" },
           { w: Infinity, v: "600px" },
@@ -96,8 +103,9 @@ export default {
       means that the brew water will stay right around the 200&deg; during
       brewing.
       <Figure
-        src="https://ye-olde-blog.s3.amazonaws.com/img/flair/9.jpg"
+        src="https://ye-olde-blog.s3.amazonaws.com/img/flair/8.jpg"
         breakpoints={[
+          { w: 800, v: "100%" },
           { w: 1000, v: "80%" },
           { w: 2000, v: "65%" },
           { w: Infinity, v: "600px" },
@@ -110,10 +118,11 @@ export default {
       >
         Lido E-T
       </a>{" "}
-      hand grinder. I've had it for a year and a half and it's been great.
+      hand grinder. I've had it for a couple years and it's been great.
       <Figure
-        src="https://ye-olde-blog.s3.amazonaws.com/img/flair/10.jpg"
+        src="https://ye-olde-blog.s3.amazonaws.com/img/flair/9.jpg"
         breakpoints={[
+          { w: 800, v: "100%" },
           { w: 1000, v: "80%" },
           { w: 2000, v: "65%" },
           { w: Infinity, v: "600px" },
@@ -123,8 +132,9 @@ export default {
       come with the Flair) to fill. At this point, the portafilter is just warm
       to the touch.
       <Figure
-        src="https://ye-olde-blog.s3.amazonaws.com/img/flair/11.jpg"
+        src="https://ye-olde-blog.s3.amazonaws.com/img/flair/10.jpg"
         breakpoints={[
+          { w: 800, v: "100%" },
           { w: 1000, v: "80%" },
           { w: 2000, v: "65%" },
           { w: Infinity, v: "600px" },
@@ -136,8 +146,9 @@ export default {
         Weiss Distribution Technique.
       </a>
       <Figure
-        src="https://ye-olde-blog.s3.amazonaws.com/img/flair/12.jpg"
+        src="https://ye-olde-blog.s3.amazonaws.com/img/flair/11.jpg"
         breakpoints={[
+          { w: 800, v: "100%" },
           { w: 1000, v: "80%" },
           { w: 2000, v: "65%" },
           { w: Infinity, v: "600px" },
@@ -145,6 +156,15 @@ export default {
       />
       Tamp the grounds and place the portafilter in the Flair, ready for
       extraction.
+      <Figure
+        src="https://ye-olde-blog.s3.amazonaws.com/img/flair/12.jpg"
+        breakpoints={[
+          { w: 800, v: "100%" },
+          { w: 1000, v: "80%" },
+          { w: 2000, v: "65%" },
+          { w: Infinity, v: "600px" },
+        ]}
+      />
       <p>
         Let's recap: We've gotten everything warmed up to temp, prepped our
         milk, prepped our beans and are ready to brew some espresso. The kettle
@@ -154,10 +174,11 @@ export default {
         away as well. The next series of moves must happen quickly in the exact
         order for the best results.
       </p>
-      <p>The timer starts when we press the boil button on our kettle.</p>
+      <p>The time crunch starts when we press the boil button on our kettle.</p>
       <Figure
         src="https://ye-olde-blog.s3.amazonaws.com/img/flair/13.jpg"
         breakpoints={[
+          { w: 800, v: "100%" },
           { w: 1000, v: "80%" },
           { w: 2000, v: "65%" },
           { w: Infinity, v: "600px" },
@@ -169,6 +190,7 @@ export default {
       <Figure
         src="https://ye-olde-blog.s3.amazonaws.com/img/flair/14.jpg"
         breakpoints={[
+          { w: 800, v: "100%" },
           { w: 1000, v: "80%" },
           { w: 2000, v: "65%" },
           { w: Infinity, v: "600px" },
@@ -181,6 +203,7 @@ export default {
       <Figure
         src="https://ye-olde-blog.s3.amazonaws.com/img/flair/15.jpg"
         breakpoints={[
+          { w: 800, v: "100%" },
           { w: 1000, v: "80%" },
           { w: 2000, v: "65%" },
           { w: Infinity, v: "600px" },
@@ -194,6 +217,7 @@ export default {
       <Figure
         src="https://ye-olde-blog.s3.amazonaws.com/img/flair/17.jpg"
         breakpoints={[
+          { w: 800, v: "100%" },
           { w: 1000, v: "80%" },
           { w: 2000, v: "65%" },
           { w: Infinity, v: "600px" },
@@ -205,6 +229,7 @@ export default {
       <Figure
         src="https://ye-olde-blog.s3.amazonaws.com/img/flair/18.jpg"
         breakpoints={[
+          { w: 800, v: "100%" },
           { w: 1000, v: "80%" },
           { w: 2000, v: "65%" },
           { w: Infinity, v: "600px" },
@@ -215,6 +240,7 @@ export default {
       <Figure
         src="https://ye-olde-blog.s3.amazonaws.com/img/flair/19.JPG"
         breakpoints={[
+          { w: 800, v: "100%" },
           { w: 1000, v: "80%" },
           { w: 2000, v: "65%" },
           { w: Infinity, v: "600px" },
@@ -223,6 +249,7 @@ export default {
       <Figure
         src="https://ye-olde-blog.s3.amazonaws.com/img/flair/20.JPG"
         breakpoints={[
+          { w: 800, v: "100%" },
           { w: 1000, v: "80%" },
           { w: 2000, v: "65%" },
           { w: Infinity, v: "600px" },
