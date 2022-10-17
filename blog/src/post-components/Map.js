9dc3ddc0dad60b400095ece2ef6162504159d2ef
Dan Filler <daniel.filler@spiff.com>
2022-10-17 11:04:11 -0400

    update map component

--- blog/src/post-components/Map.js
+++ blog/src/post-components/Map.js
@@ -1,11 +1,20 @@
+import React from "react";
 import styled from "styled-components";
 
-export const Map = styled.div`
-  width: 100%;
+const Container = styled.div`
+  width: ${({ width }) => (width ? width : "100%")};
   box-sizing: border-box;
 
   & iframe {
-    width: 100%;
-    height: 500px;
+    width: ${({ width }) => (width ? width : "100%")};
+    height: ${({ height }) => (height ? height : "500px")};
   }
 `;
+
+export const Map = ({ src, width, height }) => {
+  return (
+    <Container width={width} height={height}>
+      <iframe src={src} width="500px" height="500px"></iframe>
+    </Container>
+  );
+};
