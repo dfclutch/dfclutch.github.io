bd20c9c955b6d7d5583c60e651b715ae808ecaad
dfclutch <dwfiller@gmail.com>
2025-10-13 16:29:42 -0400

    remove old homepage code, update vite config and build

--- main/src/logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { LogoContainer } from './styles';
-
-export const Logo = ({ isMobile }) => (
-  <LogoContainer isMobile={isMobile}>
-    Dan Schiller
-  </LogoContainer>
-)
